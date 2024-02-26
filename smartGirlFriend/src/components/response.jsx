4251489ad0f380635a590abe232c0c6c5f8163e8
ASHMIT NIGAM <28896262+ashmit1294@users.noreply.github.com>
2024-02-27 00:17:37 +0530

    code spliting

--- smartGirlFriend/src/components/response.jsx
+++ smartGirlFriend/src/components/response.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const response = () => {
+const response = ({text}) => {
   return (
     <div className="mb-2">
-            <p className="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block">This is a response from the chatbot.</p>
+            <p className="bg-gray-200 text-gray-700 rounded-lg py-2 px-4 inline-block">{text}</p>
     </div>
   )
 }
 
-export default response
+export default response;
