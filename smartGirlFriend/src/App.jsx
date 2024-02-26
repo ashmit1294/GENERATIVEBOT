64d5904bce83908e19bf38293815cf53a289505a
ASHMIT NIGAM <28896262+ashmit1294@users.noreply.github.com>
2024-02-26 21:51:46 +0530

    2151uichng

--- smartGirlFriend/src/App.jsx
+++ smartGirlFriend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
-import ChatBox from './ChatBox';
+import ChatBox from './components/ChatBox';
+import ChatButton from './components/ChatButton';
+import { useState } from 'react';
 function App() {
+  const [chatOn,setChaton]=useState(false);
     return (
-    <div >
-      App
-      <ChatBox />
-      </div>
+    <div className='fixed bottom-0 right-0 mb-4 mr-4'>
+    {chatOn?<ChatBox />:<ChatButton />}
+    </div>
   )
 };
 
---
+++ smartGirlFriend/src/components/ChatButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatButton = () => {
+  return (
+    <div>
+     <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 flex items-center">
+    <abc />Chat with Bot
+  </button>
+    </div>
+  )
+}
+
+export default ChatButton
